19b0d58d08b3cde9359b5fbeb6e8ad4177bb74af
Karlo Belavic <lahur.123@gmail.com>
2026-08-25 13:59:23 +0200

    dto fixes

--- src/main/java/hr/bill/spring_bill/dto/web/bill/b2b/ReportBillRequest.java
+++ src/main/java/hr/bill/spring_bill/dto/web/bill/b2b/ReportBillRequest.java
@@ -6,42 +6,46 @@ import hr.bill.spring_bill.dto.web.bill.BaseBillRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 @Getter
+@Setter
+@NoArgsConstructor
 @Schema(description = "Request for creation of bill report")
 public class ReportBillRequest extends BaseBillRequest {
 
     @Schema(description = "Bill id", example = "1")
     @NotNull(message = "Bill id can't be null")
-    private final Integer billId;
+    private Integer billId;
 
     @Schema(description = "Bill profile", enumAsRef = true, example = "P1")
     @NotNull(message = "Bill profile can't be null")
-    private final BillProfile profile;
+    private BillProfile profile;
 
     @Schema(description = "Type of VAT used on bill", enumAsRef = true, example = "Pdv25")
     @NotNull(message = "Vat category can't be null")
-    private final VatCategory vatCategory;
+    private VatCategory vatCategory;
 
     @Schema(description = "Payment reference", example = "indent P-1-100")
     @NotNull(message = "Reference can't be null")
-    private final String reference;
+    private String reference;
 
     @Schema(description = "Buyers name", example = "PEVEX d.d.")
     @NotNull(message = "Buyer name can't be null")
-    private final String buyerName;
+    private String buyerName;
 
     @Schema(description = "Buyers residental address", example = "Savska cesta 84")
     @NotNull(message = "Buyer street can't be null")
-    private final String buyerStreet;
+    private String buyerStreet;
 
     @Schema(description = "Buyers residental city", example = "Sesvete")
     @NotNull(message = "Buyer city can't be null")
-    private final String buyerCity;
+    private String buyerCity;
 
     @Schema(description = "Buyers residental city postal code", example = "10360")
     @NotNull(message = "Buyer postal zone can't be null")
-    private final String buyerPostalZone;
+    private String buyerPostalZone;
 }
--- src/main/java/hr/bill/spring_bill/dto/web/bill/b2c/F1BillRequest.java
+++ src/main/java/hr/bill/spring_bill/dto/web/bill/b2c/F1BillRequest.java
@@ -5,30 +5,34 @@ import hr.bill.spring_bill.dto.web.bill.BaseBillRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 @Getter
+@Setter
+@NoArgsConstructor
 @Schema(description = "Request for creation of F1 bill")
 public class F1BillRequest extends BaseBillRequest {
 
     @Schema(description = "Buyers name", example = "John Doe")
     @NotNull(message = "Buyer name can't be null")
-    private final String buyerName;
+    private String buyerName;
 
     @Schema(description = "Buyers residental address", example = "Ilica 1")
     @NotNull(message = "Buyer street can't be null")
-    private final String buyerAddress;
+    private String buyerAddress;
 
     @Schema(description = "Buyers residental city", example = "Zagreb")
     @NotNull(message = "Buyer city can't be null")
-    private final String buyerCity;
+    private String buyerCity;
 
     @Schema(description = "Buyers residental city postal code", example = "10000")
     @NotNull(message = "Buyer postal zone can't be null")
-    private final String buyerPostalCode;
+    private String buyerPostalCode;
 
     @Schema(description = "Tax rate applied to the bill", enumAsRef = true, example = "Pdv25")
     @NotNull(message = "Tax rate can't be null")
-    private final TaxRate taxRate;
+    private TaxRate taxRate;
 }
